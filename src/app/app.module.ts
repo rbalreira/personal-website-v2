7e46baac08eadd3675d8da7c469ee007c8f1f1d0
Ricardo Balreira <rbalreira@outlook.com>
2023-06-30 22:53:30 +0100

    Added navigation bar + other components + styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { provideSvgIcons } from '@ngneat/svg-icon';
+import { logoIcon } from '@app/svg/logo';
+import { SvgIconComponent } from '@ngneat/svg-icon';
+
+import { HomeComponent } from './modules/home/pages/home/home.component';
+import { HeaderComponent } from './core/header/header.component';
+import { HamburgerToggleDirective } from './shared/directives/hamburger-toggle.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeComponent,
+    HeaderComponent,
+    HamburgerToggleDirective
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    SvgIconComponent
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [provideSvgIcons([logoIcon])],
+  bootstrap: [HomeComponent]
 })
 export class AppModule { }
--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
-export class AppComponent {
+export class HomeComponent {
   title = 'portfolio-website';
 }
